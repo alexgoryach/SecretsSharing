426269969e96ec838408224a2d5511f89c5fd9e8
alexgoryach <alexandr.goryach@gmail.com>
2022-12-01 01:06:37 +0700

    initial commit

---
+++ SecretsSharing.Domain/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SecretsSharing.Domain.Users
+{
+    /// <summary>
+    /// Identity user entity.
+    /// </summary>
+    public class User : IdentityUser
+    {
+        /// <summary>
+        /// User id.
+        /// </summary>
+        public Guid Id { get; private set; }
+    }
+}
---
+++ SecretsSharing.Infrastructure.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecretsSharing.Domain.Users;
+
+namespace SecretsSharing.Infrastructure.DataAccess
+{
+    /// <summary>
+    /// Application database context.
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=SecretsSharingDb;Username=postgres;Password=postgres");
+        }*/
+    }
+}

1363c1da1f76484f5820ec2a525e3ba4ea41b2f5
alexgoryach <alexandr.goryach@gmail.com>
2022-12-05 02:17:18 +0700

    added-get-all-files-method

---
+++ SecretsSharing.Usecases/Files/GetAllFiles/GetAllFilesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Saritasa.Tools.Common.Pagination;
+using SecretsSharing.Usecases.Common.Dtos.File;
+
+namespace SecretsSharing.Usecases.Files.GetAllFiles
+{
+    public record GetAllFilesQuery(int page = 1, int pageSize = 20) : IRequest<PagedListMetadataDto<FileDto>>
+    {
+    }
+}
---
+++ SecretsSharing.Usecases/Files/GetAllFiles/GetAllFilesQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Saritasa.Tools.Common.Pagination;
+using Saritasa.Tools.EFCore.Pagination;
+using SecretsSharing.Infrastructure.Abstractions.Interfaces;
+using SecretsSharing.Usecases.Common.Dtos.File;
+
+namespace SecretsSharing.Usecases.Files.GetAllFiles
+{
+    public class GetAllFilesQueryHandler : IRequestHandler<GetAllFilesQuery, PagedListMetadataDto<FileDto>>
+    {
+        private readonly IAppDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public GetAllFilesQueryHandler(IAppDbContext dbContext, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<PagedListMetadataDto<FileDto>> Handle(GetAllFilesQuery request, CancellationToken cancellationToken)
+        {
+            var query = mapper
+                .ProjectTo<FileDto>(dbContext.Files);
+            var files = await EFPagedListFactory
+                .FromSourceAsync(query, request.page, request.pageSize, cancellationToken);
+
+            return files.ToMetadataObject();
+        }
+    }
+}
